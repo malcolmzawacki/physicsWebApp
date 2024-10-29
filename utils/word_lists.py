2eaf8a625fcf4518862b2ffefff56cb519a6fb35
malcolmzawacki <115760130+malcolmzawacki@users.noreply.github.com>
2024-10-29 12:49:00 +0000

    added to object word list

--- utils/word_lists.py
+++ utils/word_lists.py
@@ -3,7 +3,8 @@ objects = ["cactus","turtle","bowl of soup","block of ice","barrel of monkeys",
            "statue","boulder","Toyota Camry","washing machine","vacant doghouse","baseball","basketball",
            "football","tennis ball","golf ball","recliner","television","record player","magic mirror",
            "broken treadmill","villainous character","stunt-seeking daredevil","Minecraft villager",
-           "mischievious rapscalion","clown","giant spider","miniature elephant","kitschy porcelain figurine"
+           "mischievious rapscalion","clown","giant spider","miniature elephant","kitschy porcelain figurine","priceless artifact",
+           "crown jewel","tuba","snare drum","block of concrete","kindergartener's action figure","bonsai tree"
            ]
 
 proj_verbs = ["catapulted","cannon-fired","launched","chucked","thrown","propelled","kaboomed into the air",
