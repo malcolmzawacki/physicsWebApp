57a07be72a1629259965ace247c723d1c858c746
malcolmzawacki <115760130+malcolmzawacki@users.noreply.github.com>
2024-10-29 11:07:19 +0000

    force new commit

--- pages/1_1.6_Type_2_Projectiles.py
+++ pages/1_1.6_Type_2_Projectiles.py
@@ -4,6 +4,8 @@ import math
 import sys
 from pathlib import Path
 
+# this comment is simply to allow a new commit
+
 # Add the project root directory to Python path
 root_dir = Path(__file__).parent.parent
 sys.path.append(str(root_dir))
