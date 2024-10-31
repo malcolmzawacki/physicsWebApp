b2ea90f9f2bf847a612faa02d9afd70c0cbc15f8
malcolmzawacki <115760130+malcolmzawacki@users.noreply.github.com>
2024-10-31 15:38:18 +0000

    changed homepage message

--- Home.py
+++ Home.py
@@ -2,8 +2,8 @@ import streamlit as st
 st.set_page_config(page_title="Home")
 st.sidebar.header("Home")
 
-st.title("Welcome to the Physics Practice Tool")
-st.write(f"Streamlit version: {st.__version__}")
+st.title("Welcome! Practice Something")
+
 
 st.write("""
 Select a problem type from the sidebar to begin practicing.
