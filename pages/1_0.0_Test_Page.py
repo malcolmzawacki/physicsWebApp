2459a76ca9f8947f35442d135624e5b739396abf
malcolmzawacki <115760130+malcolmzawacki@users.noreply.github.com>
2025-04-05 20:59:47 +0000

    is it working on main site?

--- pages/1_0.0_Test_Page.py
+++ pages/1_0.0_Test_Page.py
@@ -5,10 +5,9 @@ import streamlit as st
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
-from utils.rendering import rendering
-from utils.generators.force_generator import ForceGenerator
-
-
+from utils.ui import ui
+from utils.generators.energy_generator import EnergyGenerator
+from utils.generators.linear_motion_generator import LinearMotionGenerator
 
 
 def input_plot(start = 2.8,end = 4,accuracy=0.001,reps=600,numtoplot=200):
@@ -45,5 +44,50 @@ def main():
     fig = input_plot(start,end,accuracy,reps,numtoplot)
     st.pyplot(fig)
 
+class energy_basics:
+
+    @staticmethod
+    def question_parameters():
+        """Holds current options for questions for centralized updating"""
+        problem_type_dict = {
+                "One Dimensional": {
+                      "honors": r"""
+                      \textrm{distance } =\; d_1 + d_2 + ... \newline ~ \newline
+                      \textrm{displacement } =\; d_{final} - d_{initial} \; \textrm{(with direction)}
+                      """,
+
+                      "conceptual": r"""
+                      \textrm{distance } =\; d_1 + d_2 + ... 
+                      
+                     \newline ~ \newline \textrm{"Positive" directions are right, East, up, and North}
+                     \newline ~ \newline \textrm{"Negative" directions are left, West, down, and South}
+                     \newline ~ \newline \textrm{displacement } =\; d_{final} - d_{initial} \; \textrm{(with direction)}
+                      """},
+                "Two Dimensional": {
+                      "honors": r"""
+                      \textrm{distance } =\; d_1 + d_2 + ... \newline ~ \newline
+                    \textrm{displacement } =\; \sqrt{d_{horizontal}^2 + d_{vertical}^2} \;\; \textrm{(with direction)}
+                      """,
+
+                      "conceptual": r"""
+                      \textrm{distance } =\; d_1 + d_2 + ... \newline ~ \newline
+                      \textrm{Horizontal Displacement } =\; D_{East} - D_{West} \newline ~ \newline
+                      \textrm{Vertical Displacement } =\; D_{North} - D_{South} \newline ~ \newline
+                    \textrm{Net Displacement } =\; \sqrt{D_{horizontal}^{\;2} + D_{vertical}^2} \;\; \textrm{(with direction)}
+                      """},
+                
+                }
+        difficulties = ["Easy","Medium","Hard"]
+        return problem_type_dict, difficulties
+    
+    @staticmethod
+    def main():
+        prefix = "energy_basics"
+        title = "Types of Energy"
+        problem_type_dict, difficulties = energy_basics.question_parameters()
+        generator = LinearMotionGenerator()
+        ui = ui(prefix,title,generator,problem_type_dict,difficulties)
+        ui.diagram_layout()
+
 if __name__ == "__main__":
-    main()
+    energy_basics.main()
