e4b9babc9aa494bd940b1fe0494f6ee48f199b02
malcolmzawacki <115760130+malcolmzawacki@users.noreply.github.com>
2025-04-29 18:18:47 +0000

    changed range and decimal display

--- utils/generators/waves_generator.py
+++ utils/generators/waves_generator.py
@@ -52,7 +52,7 @@ class WaveGenerator(BaseGenerator):
         return question, [answer], [unit]
     
     def string_harmonics(self,difficulty):
-        fundamental_frequency = ri(20,20000)
+        fundamental_frequency = ri(20,5000)
         velocity = 343
         wavelength = velocity / fundamental_frequency
         string_length = wavelength / 2
@@ -61,12 +61,12 @@ class WaveGenerator(BaseGenerator):
             # only working forwards
             q_type = ri(0,3)
             if q_type == 0:
-                question = f"""What is the wavelength of the first harmonic of a {string_length:.2f} meter long string?"""
+                question = f"""What is the wavelength of the first harmonic of a {string_length:.3f} meter long string?"""
                 answer = wavelength
                 unit = "Wavelength (meters)"
 
             elif q_type == 1:
-                question = f"What is the fundamental frequency of a {string_length:.2f} meter long string?"
+                question = f"What is the fundamental frequency of a {string_length:.3f} meter long string?"
                 answer = fundamental_frequency
                 unit = "Fundamental Frequency (Hz)"
 
@@ -77,7 +77,7 @@ class WaveGenerator(BaseGenerator):
                 unit = "String Length (meters)"
 
             elif q_type == 3:
-                question = f"""How long would a string have to be to produce a wavelength of {wavelength:.2f} meters for its first harmonic?"""
+                question = f"""How long would a string have to be to produce a wavelength of {wavelength:.3f} meters for its first harmonic?"""
                 answer = string_length
                 unit = "String Length (meters)"
             return question, [answer],[unit]
@@ -85,12 +85,12 @@ class WaveGenerator(BaseGenerator):
             # still forwards, multiple answers
             q_type = ri(0,1)
             if q_type == 0:
-                question = f"""What are the wavelengths of the first three harmonics of a {string_length:.2f} meter long string?"""
+                question = f"""What are the wavelengths of the first three harmonics of a {string_length:.3f} meter long string?"""
                 answer = [wavelength, wavelength/2, wavelength/3]
                 unit = ["First Harmonic Wavelength (meters)","Second Harmonic Wavelength (meters)","Third Harmonic Wavelength (meters)"]
 
             elif q_type == 1:
-                question = f"""What are the frequencies of the first three harmonics of a {string_length:.2f} meter long string?"""
+                question = f"""What are the frequencies of the first three harmonics of a {string_length:.3f} meter long string?"""
                 answer = [fundamental_frequency, 2*fundamental_frequency, 3*fundamental_frequency]
                 unit = ["Fundamental Frequency (Hz)","Second Harmonic Frequency (Hz)","Third Harmonic Frequency (Hz)"]
 
@@ -100,12 +100,12 @@ class WaveGenerator(BaseGenerator):
             # lots of working backwards
             q_type = ri(0,1)
             if q_type == 0:
-                question = f"""The third harmonic of a string is {3*fundamental_frequency:.2f} Hertz. 
+                question = f"""The third harmonic of a string is {3*fundamental_frequency:.3f} Hertz. 
                 What is the wavelength of the first harmonic? What is the length of the string?"""
                 answer = [wavelength, string_length]
                 unit = ["Wavelength (meters)","String Length (meters)"]
             elif q_type == 1:
-                question = f"""The third harmonic of a string has a wavelength of {wavelength/3:.2f} meters. 
+                question = f"""The third harmonic of a string has a wavelength of {wavelength/3:.3f} meters. 
                 What is the fundamental frequency? What is the length of the string?"""
                 answer = [fundamental_frequency, string_length]
                 unit = ["Fundamental Frequency (Hz)","String Length (meters)"]
