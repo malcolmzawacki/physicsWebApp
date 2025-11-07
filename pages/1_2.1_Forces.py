8fd33458c27c48ad3e4ee27ee7506021434ecd08
malcolmzawacki <115760130+malcolmzawacki@users.noreply.github.com>
2025-11-07 18:58:23 +0000

    added inclined planes, moved generator import statements into tab functions for faster loading(?)

--- pages/1_2.1_Forces.py
+++ pages/1_2.1_Forces.py
@@ -2,11 +2,13 @@ import streamlit as st
 
 from tools.loading import lazy_tabs
 from utils.ui import interface
-from utils.generators.force_generator import ForceGenerator
-from utils.generators.forces.tension_generator import TensionGenerator
-from utils.generators.forces.atwood_generator import AtwoodGenerator
+
+
+
+
 
 def newtons_2nd():
+    from utils.generators.force_generator import ForceGenerator
     title = "Newton's Second Law"
     prefix = "newtons_2nd"
     difficulties = ["Easy","Medium","Hard"]
@@ -17,6 +19,7 @@ def newtons_2nd():
 
 
 def tension():
+    from utils.generators.forces.tension_generator import TensionGenerator
     title = "Tension Problems"
     prefix = "tension"
     difficulties = ["Easy","Medium","Hard"]
@@ -26,6 +29,7 @@ def tension():
     ui.unified_smart_layout()
 
 def atwood():
+    from utils.generators.forces.atwood_generator import AtwoodGenerator
     title = "Atwood Machines"
     prefix = "atwood"
     difficulties = ["Medium"]
@@ -33,6 +37,19 @@ def atwood():
     metadata = generator.stored_metadata()
     ui = interface(prefix, title, generator, metadata, difficulties)
     ui.unified_smart_layout()
+
+
+def inclines():
+    from utils.generators.forces.incline_generator import InclineGenerator
+    title = "Inclined Planes"
+    prefix = "incline"
+    difficulties = ["Medium"]
+    generator = InclineGenerator()
+    metadata = generator.stored_metadata()
+    ui = interface(prefix, title, generator, metadata, difficulties)
+    ui.unified_smart_layout()
+
+
 
 def _under_construction_tab():
     st.write("Currently Under Construction")
@@ -42,7 +59,8 @@ def main():
     tab_specs = [
         ("Newton's Second Law", newtons_2nd),
         ("Tension", tension),
-        ("Atwood Machines", atwood)
+        ("Atwood Machines", atwood),
+        ("Inclined Planes", inclines)
     ]
 
     lazy_tabs(tab_specs, state_key="forces_tabs", auto_load_first=True)
--- utils/generators/forces/incline_generator.py
+++ utils/generators/forces/incline_generator.py
@@ -11,7 +11,11 @@ class InclineGenerator(BaseGenerator):
     
     def choose_problem_dict(self, problem_type, difficulty):
         if problem_type == "Static Incline":
-            return
+            return self.static_incline()
+        elif problem_type == "Frictionless Incline":
+            return self.frictionless_incline()
+        elif problem_type == "Kinetic Friction Incline":
+            return self.kinetic_friction_incline()
     
     
     def stored_metadata(self) -> dict[str, dict]:
@@ -54,3 +58,100 @@ class InclineGenerator(BaseGenerator):
                       }"""
             }
         }
+    
+    def incline_nums(self):
+        theta = ri(1,44)
+        mu_max = math.tan(math.pi*theta/180)
+        accel_no_mu = 10*math.sin(math.pi*theta/180)
+        mu_k = ri(1, math.floor(100*mu_max)-1)/100
+        accel_mu_k = accel_no_mu - 10*mu_k*math.cos(math.pi*theta/180)
+        return {
+            "angle": theta,
+            "static coeff": mu_max,
+            "accel w/o frict": accel_no_mu,
+            "kinetic coeff": mu_k,
+            "accel w/ frict": accel_mu_k
+        }
+
+    def static_incline(self, solve_for = None):
+        """solve for options are: angle, or static coeff"""
+        if solve_for == None:
+            solve_for = random.choice([
+                "angle",
+                "static coeff"
+            ])
+        q_nums = self.incline_nums()
+        noun = random_noun()
+        mass = ri(1,25)
+        if solve_for == "angle":
+            question = f"""A {mass} kg {noun} sits at rest on an inclined plane.
+            The coefficient of static friction between the {noun} and the plane is {q_nums["static coeff"]:.2f}.
+            What is the angle of the inclined plane?"""
+            answer = [q_nums["angle"]]
+            unit = ["Angle (degrees)"]
+        else:
+            question = f"""A {mass} kg {noun} sits at rest on an inclined plane.
+            The plane sits at an angle of {q_nums["angle"]} degrees.
+            What is the coefficient of static friction between the {noun} and the plane?"""
+            answer = [q_nums["static coeff"]]
+            unit = ["Coefficient of Static Friction"]
+        return {"question": question, "answers": answer, "units": unit}
+    
+    def frictionless_incline(self, solve_for = None):
+        """solve for options are: angle, or accel"""
+        if solve_for == None:
+            solve_for = random.choice([
+                "angle",
+                "accel"
+            ])
+        q_nums = self.incline_nums()
+        accel = q_nums["accel w/o friction"]
+        noun = random_noun()
+        mass = ri(1,25)
+        if solve_for == "angle":
+            question = f"""A {mass} kg {noun} accelerates down an inclined plane at a rate of {accel:.2f}.
+            What is the angle of the inclined plane?"""
+            answer = [q_nums["angle"]]
+            unit = ["Angle (degrees)"]
+        else:
+            question = f"""A {mass} kg {noun} accelerates down an inclined plane.
+            The plane sits at an angle of {q_nums["angle"]} degrees.
+            What is the acceleration of the {noun} down the plane?"""
+            answer = [accel]
+            unit = [f"{noun}'s acceleration (m/s^2)"]
+        return {"question": question, "answers": answer, "units": unit}
+    
+        
+    def kinetic_friction_incline(self, solve_for = None):
+        """solve for options are: angle, accel, coeff"""
+        if solve_for == None:
+            solve_for = random.choice([
+                "angle",
+                "accel",
+                "coeff"
+            ])
+        q_nums = self.incline_nums()
+        accel = q_nums["accel w/ friction"]
+        coeff = q_nums["kinetic coeff"]
+        noun = random_noun()
+        mass = ri(1,25)
+        if solve_for == "angle":
+            question = f"""A {mass} kg {noun} accelerates down an inclined plane at a rate of {accel:.2f}.
+            The coefficient of kinetic friction between the {noun} and the plane is {coeff:.2f}.
+            What is the angle of the inclined plane?"""
+            answer = [q_nums["angle"]]
+            unit = ["Angle (degrees)"]
+        elif solve_for == "accel":
+            question = f"""A {mass} kg {noun} accelerates down an inclined plane.
+            The plane sits at an angle of {q_nums["angle"]} degrees.
+            The coefficient of kinetic friction between the {noun} and the plane is {coeff:.2f}.
+            What is the acceleration of the {noun} down the plane?"""
+            answer = [accel]
+            unit = [f"{noun}'s acceleration (m/s^2)"]
+        else:
+            question = f"""A {mass} kg {noun} accelerates down an inclined plane at a rate of {accel:.2f}.
+            The plane sits at an angle of {q_nums["angle"]} degrees.
+            What is the coefficient of kinetic friction between the {noun} and the plane?"""
+            answer = [coeff]
+            unit = ["Coefficient of Kinetic Friction"]
+        return {"question": question, "answers": answer, "units": unit}
